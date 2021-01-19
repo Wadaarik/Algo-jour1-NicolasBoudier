0065ccafb0aa47ea3b88d41ab27d41fd8c4afe01
Loituma <27847873+Loituma@users.noreply.github.com>
2021-01-19 21:22:28 +0100

    add some features

--- Jour2/block.py
+++ Jour2/block.py
@@ -1,5 +1,6 @@
 from hashlib import sha256
 from datetime import datetime
+import json
 
 
 class Block:
@@ -7,21 +8,26 @@ class Block:
 
     def __init__(self, data):
         self.data = data
-        self.timestamp = datetime.now()
+        self.timestamp = str(datetime.now())
         self.nonce = 0
         self.hashActu = Block.calculateHash(self)
         self.previousHash = Block.prevHash
         Block.prevHash = self.hashActu
 
-    def getDatetime(self):
-        print(self.timestamp)
-
     def getData(self):
-        print(self.data)
+        return self.data
+
+    def getDatetime(self):
+        return self.timestamp
+
+    def getNonce(self):
+        return self.nonce
 
     def getHashActu(self):
-        # print(self.hashActu)
         return self.hashActu
+
+    def getPreviousHash(self):
+        return self.previousHash
 
     def calculateHash(self):
         bloc = str(Block.prevHash) + str(self.timestamp) + str(self.data) + str(self.nonce)
@@ -38,18 +44,30 @@ class Block:
 class Blockchain():
     def __init__(self, zero):
         self.block = []
-        self.zero = '0'*zero
+        self.zero = '0' * zero
         self.validity = False
 
     def addBlock(self, aBlock):
         self.block.append(aBlock)
 
+    # Check que le hash precedent a le bon nbe de 0
+    # Check si le hash actuel a le bon nbe de 0
+    # Check si le hash precedent est bien equivaut au hash du block precedent
     def validityBlock(self):
         for index, ablock in enumerate(self.block):
-            if ablock.prevHash[:3] == self.zero and index != 0 \
-                    and ablock.hashActu[:3] == self.zero \
-                    and ablock.previousHash == self.block[index - 1].getHashActu():
-                self.validity = True
+            if index != 0:
+                if ablock.prevHash[:3] == self.zero \
+                        and ablock.hashActu[:3] == self.zero \
+                        and ablock.previousHash == self.block[index - 1].getHashActu() \
+                        and ablock.data \
+                        and ablock.timestamp:
+                    self.validity = True
+            else:
+                if ablock.prevHash[:3] == self.zero \
+                        and ablock.hashActu[:3] == self.zero \
+                        and ablock.data \
+                        and ablock.timestamp:
+                    self.validity = True
 
         return self.validity
 
@@ -60,18 +78,36 @@ class Blockchain():
             print('    Timestamp : ' + str(ablock.timestamp))
             print('    Data : ' + ablock.data)
             print('    Hash : ' + str(ablock.hashActu))
+            print('    Nonce : ' + str(ablock.nonce))
             print('] \n')
 
+    def deleteLastBlock(self):
+        self.block.pop()
 
-block1 = Blockchain(3)
+    def saveBlockchain(self):
+        dateUse = str(datetime.now().strftime("%y%m%d-%H_%M_%S"))
+        print('Le fichier est : ' + dateUse + '.json')
+        json_file = open("saveBlockchain-" + dateUse + '.json', "w")
+        json_file.write(json.dumps(self.block, default=lambda x: x.__dict__))
+        json_file.close()
 
-b0 = Block("loituma")
-b1 = Block("gerard")
-b2 = Block("testos")
 
-block1.addBlock(b0)
-block1.addBlock(b1)
-block1.addBlock(b2)
+blockchain = Blockchain(3)
 
-print('Blockchain validity: ' + str(block1.validityBlock()))
-block1.getBlockchain()
+nbBlock = int(input('Combien de block voulez-vous créer ? '))
+
+for x in range(nbBlock):
+    data = input('Veuillez rentrer une donnée pour le block n°' + str(x + 1) + ' : ')
+    b = Block(data)
+    blockchain.addBlock(b)
+
+    # toDeleteLastBlock = input('Voulez-vous supprimer le dernier block ? (y/n) ')
+    # if toDeleteLastBlock == 'y' or toDeleteLastBlock == 'yes':
+    #     blockchain.deleteLastBlock()
+
+toSave = input('\nVoulez-vous sauvegarder la blockchain dans un ficher ? (y/n) ')
+if toSave == 'y' or toSave == 'yes':
+    blockchain.saveBlockchain()
+
+print('\nBlockchain validity: ' + str(blockchain.validityBlock()))
+blockchain.getBlockchain()
